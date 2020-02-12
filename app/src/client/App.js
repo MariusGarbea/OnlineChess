2c1c24f0105b5b48f18d631d88ef74b6ccfd7f63
cassandrale179 <lenguyetminh1998@gmail.com>
2020-02-12 15:34:50 -0500

    App.js

--- app/src/client/App.js
+++ app/src/client/App.js
@@ -5,8 +5,9 @@ import Chess from "chess.js";
 
 export default class App extends Component {
   state = { 
-    username: null, 
     chess: null, 
+    ascii: null, 
+    gameStatus: 'Player move', 
   };
 
   componentDidMount() {
@@ -16,16 +17,50 @@ export default class App extends Component {
   render() {
     /* Initialize a new game of chess here */ 
     this.chess = new Chess(); 
+    this.validateMove('f3', this.ascii); 
+    this.fenParser(); 
     return (
       <div>
-        <p>Hello World!</p>
-        
+        <p>Chess Board</p>
       </div>
     );
   }
 
-
-  makeMove(move){
+  /** Function to validate move and update the board */
+  validateMove(move, boardStr){
     this.chess.move(move); 
+    this.ascii = this.chess.ascii(); 
+    if (this.chess.in_threefold_repetition()){
+      this.gameStatus = 'Drawn due to threefold repetition';  
+    } else if (this.chess.insufficient_material()){
+      this.gameStatus = 'Drawn due to insufficient material'; 
+    } else if (this.chess.game_over()){
+      this.gameStatus = 'Checkmate!'; 
+    } else if (boardStr == this.ascii){
+      console.log(boardStr); 
+      this.gameStatus = 'Invalid move!'; 
+    } else {
+      this.gameStatus = 'Valid move!'; 
+    }
   }
+
+  /** Parse FEN notation in chess */
+  fenParser(){
+    Array.matrix = function(numrows, numcols, initial){
+      var arr = [];
+      for (var i = 0; i < numrows; ++i){
+         var columns = [];
+         for (var j = 0; j < numcols; ++j){
+            columns[j] = initial;
+         }
+         arr[i] = columns;
+       }
+       return arr;
+   }
+    let board = Array.matrix(8,8,0);
+    console.log(board); 
+
+  }
+
+  
 }
