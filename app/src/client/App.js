8450ca782ddeffe38957892e850334313fbc2bd7
cassandrale179 <lenguyetminh1998@gmail.com>
2020-02-12 17:03:55 -0500

    parse fen, in progress

--- app/src/client/App.js
+++ app/src/client/App.js
@@ -8,17 +8,64 @@ export default class App extends Component {
     chess: null, 
     ascii: null, 
     gameStatus: 'Player move', 
+    board: null, 
   };
 
   componentDidMount() {
-  
+    Array.matrix = function(numrows, numcols, initial){
+      var arr = [];
+      for (var i = 0; i < numrows; ++i){
+         var columns = [];
+         for (var j = 0; j < numcols; ++j){
+            columns[j] = initial;
+         }
+         arr[i] = columns;
+       }
+       return arr;
+   }
+    this.board = Array.matrix(8,8,'X'); 
+    this.chess = new Chess(); 
+    this.board = this.initializeBoard(this.chess.fen(), this.board); 
+
   }
+
+  initializeBoard(fen, board){
+    fen = 'rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2'; 
+    let whitespace = fen.substr(0,fen.indexOf(' ')); 
+    let rows = whitespace.split('/').slice(0,8); 
+
+    // 4p3: convert FEN move to 4 empty space + pawn + 3 empty space 
+    rows.forEach((row,i) => {
+      let newRow = ''; 
+      for (let i = 0; i < row.length; i++){
+        if (isNaN(row[i])){
+          newRow += row[i]; 
+        } else {
+          let empty = 'X'.repeat(parseInt(row[i])); 
+          newRow += empty; 
+        }
+      }
+       rows[i] = newRow; 
+    })
+
+    // Input new information onto the board 
+    let piece = {name: "", color: ""}; 
+    let names = {
+      "p": "pawn", 
+      "n": "knight", 
+      "b": "bishop", 
+      "r": "rook", 
+      "q": "queen", 
+      "k": "king"
+    }
+
+    
+ }
 
   render() {
     /* Initialize a new game of chess here */ 
-    this.chess = new Chess(); 
-    this.validateMove('f3', this.ascii); 
-    this.fenParser(); 
+    // this.validateMove('f3', this.ascii); 
+    // this.fenParser(); 
     return (
       <div>
         <p>Chess Board</p>
@@ -44,21 +91,12 @@ export default class App extends Component {
     }
   }
 
+
+
   /** Parse FEN notation in chess */
-  fenParser(){
-    Array.matrix = function(numrows, numcols, initial){
-      var arr = [];
-      for (var i = 0; i < numrows; ++i){
-         var columns = [];
-         for (var j = 0; j < numcols; ++j){
-            columns[j] = initial;
-         }
-         arr[i] = columns;
-       }
-       return arr;
-   }
-    let board = Array.matrix(8,8,0);
-    console.log(board); 
+  fenParser(move){
+    let dict = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h'}; 
+  
 
   }
 
