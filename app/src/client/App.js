daf201474c6ca58920f255fc41bb2df5cfc37536
hunter-heidenreich <hunter.scott.heidenreich@gmail.com>
2020-02-16 16:28:44 -0500

    Set up basic socket communication. Logs when a user connects/disconnects

--- app/src/client/App.js
+++ app/src/client/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
+
 import './app.css';
-import Board from './Board'; 
- 
+import Board from './Board';
 
 export default class App extends Component {
-  
+
+  constructor() {
+    super();
+
+    self.socket = io('/socket');
+  }
 
   render() {
     return (
-      <Board /> 
+      <Board />
     )
   }
-
-
 
 }
