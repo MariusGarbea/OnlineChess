ffe9bd6bce528dd6222645d2c5295da17ff77bf4
Saloni Purswani <salonipurswani@Salonis-MBP.fios-router.home>
2020-03-02 00:02:22 -0500

    added piece movement

---
+++ app/src/client/Piece.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./app.css";
+
+export default class Piece extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            unicodeLabel: 
+            this.convertStringToUnicode(this.props.label)
+        };
+    }
+
+    convertStringToUnicode(label){
+        let string_to_unicode = {
+            'K': '♔', 'Q': '♕', 'R': '♖', 
+            'B': '♗', 'N': '♘', 'P': '♙', 
+            'k': '♚', 'q': '♛', 'r': '♜', 
+            'b': '♝', 'n': '♞', 'p': '♟'
+          };
+        return string_to_unicode[label];
+    }
+
+    render(){
+        return this.state.unicodeLabel;
+    }
+}
