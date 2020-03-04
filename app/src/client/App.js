8a280181d47c112248d6501407e6d2715dd5d1e5
cassandrale179 <lenguyetminh1998@gmail.com>
2020-03-04 18:10:59 -0500

    Addi n status

--- app/src/client/App.js
+++ app/src/client/App.js
@@ -25,52 +25,52 @@ export default class App extends Component {
 
   }
 
-  componentDidMount() {
-    // Connects app to sockets on the backend
-    // self.socket = io('http://localhost:3200');
-    // self.socket.on('connect', () => {
-    //   console.log(`Connected. ID: ${self.socket.id}`);
-    // });
-    // // Example of registration of username
-    // let name = prompt('Enter a username: ');
-    // self.socket.emit('/register', {'name': name}, resp => {
-    //     if (resp) {
-    //       alert('Name successfully registered!');
-    //     } else {
-    //       alert('Name registration failed. Name already exists.');
-    //     }
-    //   }
-    // );
-    // this.setState({name});
+  handleSocketConnection(){
+     // Connects app to sockets on the backend 
+    self.socket = io('http://localhost:3200');
+    self.socket.on('connect', () => {
+      console.log(`Connected. ID: ${self.socket.id}`);
+    });
+    // Example of registration of username
+    let name = prompt('Enter a username: ');
+    self.socket.emit('/register', {'name': name}, resp => {
+        if (resp) {
+          alert('Name successfully registered!');
+        } else {
+          alert('Name registration failed. Name already exists.');
+        }
+      }
+    );
+    this.setState({name});
 
-    // // Handler for getting a game request
-    // self.socket.on('receiveRequest', (data) => {
-    //   let res = confirm(`Player "${data.playerName}" would like to player a game!`);
+    // Handler for getting a game request 
+    self.socket.on('receiveRequest', (data) => {
+      let res = confirm(`Player "${data.playerName}" would like to player a game!`);
 
-    //   if (res) {
-    //     self.socket.emit('/acceptMatch', {
-    //       'p1': self.name,
-    //       'p2': data.playerName
-    //     });
-    //   } else {
-    //     self.socket.emit('/rejectMatch', {
-    //       'p1': self.name,
-    //       'p2': data.playerName
-    //     });
-    //   }
-    // });
+      if (res) {
+        self.socket.emit('/acceptMatch', {
+          'p1': self.name,
+          'p2': data.playerName
+        });
+      } else {
+        self.socket.emit('/rejectMatch', {
+          'p1': self.name,
+          'p2': data.playerName
+        });
+      }
+    });
 
-    // self.socket.on('rejected', (data) => {
-    //   alert(`Player ${data.playerName} has rejected your game request :(`);
-    // });
+    self.socket.on('rejected', (data) => {
+      alert(`Player ${data.playerName} has rejected your game request :(`);
+    });
 
-    // self.socket.on('accepted', (data) => {
-    //   alert(`Player ${data.playerName} has accepted your game request!`);
-    // });
+    self.socket.on('accepted', (data) => {
+      alert(`Player ${data.playerName} has accepted your game request!`);
+    });
 
-    // self.socket.on('update', (data) => {
-    //   console.log(data.fen);
-    // });
+    self.socket.on('update', (data) => {
+      console.log(data.fen);
+    });
 
     // Example of registration of username
     // let name = null;
@@ -93,36 +93,39 @@ export default class App extends Component {
     // }
 
     // Example of getting list of players
-    // self.socket.emit('/getPlayers', {}, resp => {
-    //   console.log(resp);
-    //   this.setState({players: resp});
-    // });
-    // setTimeout(() => {
-    //   this.setState({gameAccepted: true})
-    // }, 2000)
+    self.socket.emit('/getPlayers', {}, resp => {
+      console.log(resp);
+      this.setState({players: resp});
+    });
+    setTimeout(() => {
+      this.setState({gameAccepted: true})
+    }, 2000)
+
+  }
+
+  componentDidMount() {
+    this.handleSocketConnection(); 
   }
 
   render() {
+
     return (
-      <Board />
-    )
-    // return (
-    //   <Router>
-    //     <div>
-    //       <Switch>
-    //         <Route exact path="/">
-    //           {this.state.gameAccepted ?
-    //             <Redirect to="/game" />:
-    //             <Menu name={this.state.name} players={this.state.players} />
-    //           }
-    //         </Route>
-    //         <Route path="/game">
-    //           <Board />
-    //         </Route>
-    //       </Switch>
-    //     </div>
-    //   </Router>
-    // );
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              {this.state.gameAccepted ?
+                <Redirect to="/game" />:
+                <Menu name={this.state.name} players={this.state.players} />
+              }
+            </Route>
+            <Route path="/game">
+              <Board />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 
 }
