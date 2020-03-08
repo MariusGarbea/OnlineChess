8da91e3e3ee7b221f44f2b620e5701bb7e056319
cassandrale179 <lenguyetminh1998@gmail.com>
2020-03-08 15:40:57 -0400

    Add second test

--- app/src/client/App.mock.js
+++ app/src/client/App.mock.js
@@ -1,11 +1,4 @@
-
-import Chess from "chess.js";
-
-// Initialize functions here 
-let chess = new Chess(); 
-
-
-export default function createEmptyMatrix(numrows, numcols, initial) {
+function createEmptyMatrix(numrows, numcols, initial) {
   var arr = [];
   for (var i = 0; i < numrows; ++i) {
     var columns = [];
@@ -17,5 +10,38 @@ export default function createEmptyMatrix(numrows, numcols, initial) {
   return arr;
 }
 
+function initializeBoard() {
+    let board = createEmptyMatrix(8, 8, 0); 
+    
+    // Take only the first part of the FEN representation
+    // E.g: rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR
+    let fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'; 
+    let whitespace = fen.substr(0, fen.indexOf(" "));
+    let rows = whitespace.split("/").slice(0, 8);
 
-export { createEmptyMatrix }; 
+    // For each row of the FEN representation, parse empty spaces
+    // and convert them to 'X' to represent the empty space
+    rows.forEach((row, i) => {
+      let newRow = "";
+      for (let i = 0; i < row.length; i++) {
+        if (isNaN(row[i])) {
+          newRow += row[i];
+        } else {
+          let empty = "X".repeat(parseInt(row[i]));
+          newRow += empty;
+        }
+      }
+      rows[i] = newRow;
+    });
+
+    // For each row, return the board
+    rows.forEach((row, i) => {
+      row = [...row];
+      row.forEach((col, j) => {
+        board[i][j] = col;
+      });
+    });
+    return board; 
+  }
+
+export { createEmptyMatrix, initializeBoard }; 
