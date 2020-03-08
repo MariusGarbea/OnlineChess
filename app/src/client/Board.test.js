94f52488f4972848142817f339d7ac321a7987a4
brahmnoor <bsc73@drexel.edu>
2020-03-08 14:17:02 -0400

    adding a basic boiler plate code for the tests for the board

---
+++ app/src/client/Board.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+it('should have a valid chessboard', () => {
+  const board = shallow(<Board />);
+  expect(board.props().board).toEqual(
+    [['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']]);
+});
