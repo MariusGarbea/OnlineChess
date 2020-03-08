f137870688fb6052259002870cf7a9786d24547e
cassandrale179 <lenguyetminh1998@gmail.com>
2020-03-08 15:06:34 -0400

    Test empty board

--- app/src/client/App.spec.js
+++ app/src/client/App.spec.js
@@ -1,5 +1,31 @@
-describe('My Test Suite', () => {
-  it('My Test Case', () => {
-    expect(true).toEqual(true);
+
+function createEmptyMatrix(numrows, numcols, initial) {
+  var arr = [];
+  for (var i = 0; i < numrows; ++i) {
+    var columns = [];
+    for (var j = 0; j < numcols; ++j) {
+      columns[j] = initial;
+    }
+    arr[i] = columns;
+  }
+  return arr;
+}
+
+
+describe('Board.js Test Suite', () => {
+  it('Initialize empty matrix', () => {
+    let emptyBoard = [ 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]
+  ]; 
+  // console.log(emptyBoard); 
+  // console.log(createEmptyMatrix(8,8,0)); 
+    expect(createEmptyMatrix(8,8,0)).toStrictEqual(emptyBoard);    
   });
 });
